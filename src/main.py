135f0a545d86ceec655eb95f4e0dc75736050e53
Santiago Puerta <puertasantiago2002@gmail.com>
2024-10-27 23:27:57 -0500

    refactor: Changed PORT from 8080 to 9092

--- src/main.py
+++ src/main.py
@@ -13,4 +13,4 @@ def create_app() -> Flask:
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 9092)))
