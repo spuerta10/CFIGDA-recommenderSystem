683205a20abd9862d658abfc9678febf32be126b
spuerta10 <puertasantiago2002@gmail.com>
2024-11-08 18:53:32 -0500

    style & docs: Added pydoc to methods and auto formatted by black

--- src/api/controller/controller.py
+++ src/api/controller/controller.py
@@ -1,16 +1,17 @@
+"""
+API module providing an endpoint for item recommendations based on customer 
+purchase history. Uses KNN modeling to analyze and suggest items for users.
+"""
+
+from flask import Blueprint, Response, request, jsonify
+
 from api.knn_model import KNNModel
 from api.controller.request_parser import RequestParser
-
-from flask import (
-    Blueprint,
-    Response,
-    request,
-    jsonify
-)
 
 
 controller_bp = Blueprint("controller", __name__)
 knn_model = KNNModel(r"src/api/conf/knn_model_conf.json")
+
 
 @controller_bp.route("/items/recommend", methods=["POST"])
 def recommend_items() -> Response:
@@ -20,18 +21,22 @@ def recommend_items() -> Response:
         ValueError: If the JSON request is empty or missing required fields.
 
     Returns:
-        Response: JSON response containing recommended items or an error message if an exception occurs.
+        Response: JSON response containing recommended items 
+        or an error message if an exception occurs.
     """
     try:
         json_request = request.get_json(silent=True)
-        if json_request is None: 
+        if json_request is None:
             raise ValueError("The given request is empty!")
         RequestParser(**json_request)
         recommended_items: list[int] = knn_model.recommend(
-            customer_id= json_request["customer_id"],
-            order_items= json_request["order_items"],
-            num_recommendations= json_request["num_recommendations"] \
-                if json_request.get("num_recommendations") else 3
+            customer_id=json_request["customer_id"],
+            order_items=json_request["order_items"],
+            num_recommendations=(
+                json_request["num_recommendations"]
+                if json_request.get("num_recommendations")
+                else 3
+            ),
         )
         return jsonify({"recommended_items": recommended_items})
     except Exception as e:
